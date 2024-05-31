9a8aba4f6b6448db73b6f8bdf07eb92c3cd93928
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-31 19:21:02 -0300

    Verificando valor no campo nome

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 var campoNome = document.getElementById("nome");
+var campoNascimento = document.getElementById("ano");
 
 campoNome.onkeyup = function() {
     let nome = campoNome.value;
     if (nome.length == 0) {
-        alert("Digite seu nome!")
+        campoNome.classList.remove("campo-valido");
+        campoNome.classList.add("campo-invalido");
+        alert("DDD");
+    } else {
+        campoNome.classList.remove("campo-invalido");
+        campoNome.classList.add("campo-valido");
     }
 }
