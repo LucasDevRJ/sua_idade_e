a9a60daf83e78ee55d669b8d54fd22876dbe07cb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-31 19:39:57 -0300

    Mensagem de erro personalizade

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ var campoNascimento = document.getElementById("ano");
 campoNome.onkeyup = function() {
     let nome = campoNome.value;
     if (nome.length == 0) {
+        var mensagemErro = document.getElementById("erro-nome");
+        var p = document.createElement("p");
+        var texto = document.createTextNode("Digite seu nome!");
+        p.appendChild(texto);
+        mensagemErro.appendChild(p);
+
+        p.classList.add("mensagem-erro");
         campoNome.classList.remove("campo-valido");
         campoNome.classList.add("campo-invalido");
         alert("DDD");
