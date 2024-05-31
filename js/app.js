4bea7836bb31e2308ada429ded89bd6c16e67dc1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-31 19:55:46 -0300

    Mensagem de erro para o nome

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 var campoNome = document.getElementById("nome");
 var campoNascimento = document.getElementById("ano");
+var mensagemErroNome = document.getElementById("erro-nome");
+
+document.body.onload = function() {
+    mensagemErroNome.style.visibility = "hidden";
+}
 
 campoNome.onkeyup = function() {
     let nome = campoNome.value;
     if (nome.length == 0) {
-        var mensagemErro = document.getElementById("erro-nome");
-        var p = document.createElement("p");
-        var texto = document.createTextNode("Digite seu nome!");
-        p.appendChild(texto);
-        mensagemErro.appendChild(p);
-
-        p.classList.add("mensagem-erro");
         campoNome.classList.remove("campo-valido");
         campoNome.classList.add("campo-invalido");
-        alert("DDD");
+        mensagemErroNome.classList.add("mensagem-erro");
+        mensagemErroNome.style.visibility = "visible";
+        mensagemErroNome.innerHTML = "Digite seu nome!";
     } else {
         campoNome.classList.remove("campo-invalido");
         campoNome.classList.add("campo-valido");
